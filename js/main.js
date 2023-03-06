11c1b76cc3f58178a9c0c9203a8f5bb7c2d20c7c
guiyee <guillermodinanno@gmail.com>
2023-03-06 17:13:31 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const hero = document.querySelector('.hero');
 
 if (CSS.supports('background-blend-mode', 'multiply')) {
-  hero.style.backgroundImage = 'url(/img/shoe-3.png), radial-gradient(#747070, #111)';
+  hero.style.backgroundImage = 'url(../img/shoe-3.png), radial-gradient(#747070, #111)';
   hero.style.backgroundBlendMode = 'multiply';
 } else {
   hero.style.backgroundColor = '#222';
