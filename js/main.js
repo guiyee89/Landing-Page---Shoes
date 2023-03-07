9bbc2fbb694280df02f20500e5367d306953334b
guiyee <guillermodinanno@gmail.com>
2023-03-07 16:03:49 -0300

    finished

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 if (CSS.supports('background-blend-mode', 'multiply')) {
   hero.style.backgroundImage = 'url(../img/shoe-3.png), radial-gradient(#747070, #111)';
   hero.style.backgroundBlendMode = 'multiply';
+
 } else {
   hero.style.backgroundColor = '#222';
-} */
+}
+
+ */
